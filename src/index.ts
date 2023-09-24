9ba450d541ec6ce8ee56ffbf0e1c45d78cfceb36
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-09-24 20:45:30 +0800

    chore: 实现vue2 exportdefault模板的切换后树节点的更新

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getActiveText, getActiveTextEditorLanguageId } from '@vscode-use/utils'
+import { addEventListener, getActiveText, getActiveTextEditorLanguageId } from '@vscode-use/utils'
 import { parse } from '@vue/compiler-sfc'
 import { parse as tsParser } from '@typescript-eslint/typescript-estree'
 import type { ExtensionContext } from 'vscode'
@@ -6,27 +6,41 @@ import { parserDefault, parserSetup } from './vue'
 import { renderTree } from './treeProvider'
 
 export function activate(context: ExtensionContext) {
-  const lan = getActiveTextEditorLanguageId()
-  const code = getActiveText()
-  if (!code)
-    return
-  switch (lan) {
-    case 'vue': {
-      const { descriptor: { styles, template, script, scriptSetup }, errors } = parse(code)
-      if (errors.length)
-        return
-      let data
-      if (scriptSetup)
-        data = parserSetup(tsParser(scriptSetup.content, { jsx: true, loc: true }))
-      else if (script)
-        data = parserDefault(tsParser(script.content, { jsx: true, loc: true }))
+  let treeProvider: any = null
+  const updateTree = () => {
+    const lan = getActiveTextEditorLanguageId()
+    const code = getActiveText()
+    if (!code)
+      return
+    switch (lan) {
+      case 'vue': {
+        const { descriptor: { styles, template, script, scriptSetup }, errors } = parse(code)
+        if (errors.length)
+          return
+        let data
+        if (scriptSetup)
+          data = parserSetup(tsParser(scriptSetup.content, { jsx: true, loc: true }))
+        else if (script)
+          data = parserDefault(tsParser(script.content, { jsx: true, loc: true }))
 
-      if (!data)
-        return
-      // 1.将数据渲染到侧边栏，以树形式，展示methods，props，computed；2. 监听点击事件，跳转对应代码行数，
-      renderTree({ ...data, baseLine: (script || scriptSetup)?.loc.start.line }, context)
+        if (!data)
+          return
+        // 1.将数据渲染到侧边栏，以树形式，展示methods，props，computed；2. 监听点击事件，跳转对应代码行数，
+        if (!treeProvider)
+          treeProvider = renderTree({ ...data, baseLine: (script || scriptSetup)?.loc.start.line }, context)
+        else
+          treeProvider.update({ ...data, baseLine: (script || scriptSetup)?.loc.start.line })
+      }
     }
   }
+  updateTree()
+  context.subscriptions.push(addEventListener('activeText-change', () => {
+    updateTree()
+  }))
+
+  context.subscriptions.push(addEventListener('text-save', () => {
+    updateTree()
+  }))
 }
 
 export function deactivate() {
--- src/treeProvider.ts
+++ src/treeProvider.ts
@@ -1,44 +1,44 @@
-import { TreeProvider } from '@vscode-use/treeprovider'
+import { renderTree as render } from '@vscode-use/treeprovider'
 import { jumpToLine, registerCommand } from '@vscode-use/utils'
 import type { TreeData } from '@vscode-use/treeprovider'
 import type { ExtensionContext } from 'vscode'
 import * as vscode from 'vscode'
 
 export function renderTree(data: any, context: ExtensionContext) {
-  const { methods, computed, baseLine } = data
+  const treeData: TreeData = generateTreeData(data)
+  context.subscriptions.push(registerCommand('function-quick-locking.jump', (data, baseLine) => {
+    jumpToLine(data.start.line + baseLine - 1)
+  }))
+  const { update } = render(treeData, 'function-quick-locking.id')
+
+  return {
+    update(data: any) {
+      const newTreeData = generateTreeData(data)
+      update(newTreeData)
+    },
+  }
+}
+
+function generateTreeData(data: any) {
   const treeData: TreeData = []
-  // treeData.push({
-  //   label: 'methods',
-  //   collapsed: true,
-  //   iconPath:new vscode.ThemeIcon('symbol-module'),
-  //   children: Object.keys(methods).map(method => {
-  //     return {
-  //       label: method,
-  //       iconPath: new vscode.ThemeIcon('symbol-method'),
-  //       command: {
-  //         title: method,
-  //         command: 'function-quick-locking.jump',
-  //         arguments: [methods[method].locs],
-  //       }
-  //     }
-  //   })
-  // })
-  treeData.push({
-    label: 'methods',
-    collapsed: true,
-    iconPath: new vscode.ThemeIcon('symbol-module'),
-    children: methods.value.map((item: any) => {
-      const label = item.key.name
-      return {
-        label,
-        iconPath: new vscode.ThemeIcon('symbol-method'),
-        command: {
-          title: label,
-          command: 'function-quick-locking.jump',
-          arguments: [item.loc, baseLine],
-        },
-      }
-    }),
+  const { methods, computed, baseLine } = data
+  if (methods) {
+    treeData.push({
+      label: 'methods',
+      collapsed: true,
+      iconPath: new vscode.ThemeIcon('symbol-module'),
+      children: methods.value.map((item: any) => {
+        const label = item.key.name
+        return {
+          label,
+          iconPath: new vscode.ThemeIcon('symbol-method'),
+          command: {
+            title: label,
+            command: 'function-quick-locking.jump',
+            arguments: [item.loc, baseLine],
+          },
+        }
+      }),
 
     // Object.keys(methods).map(method => {
     //   return {
@@ -51,28 +51,29 @@ export function renderTree(data: any, context: ExtensionContext) {
     //     }
     //   }
     // })
-  })
+    })
+  }
+  if (computed) {
+    treeData.push({
+      label: 'computed',
+      collapsed: true,
+      iconPath: new vscode.ThemeIcon('symbol-module'),
+      children: computed.value.map((item: any) => {
+        const label = item.type === 'SpreadElement'
+          ? item.argument.callee.name
+          : item.key.name
+        return {
+          label,
+          iconPath: new vscode.ThemeIcon('symbol-method'),
+          command: {
+            title: label,
+            command: 'function-quick-locking.jump',
+            arguments: [item.loc, baseLine],
+          },
+        }
+      }),
+    })
+  }
 
-  treeData.push({
-    label: 'computed',
-    collapsed: true,
-    iconPath: new vscode.ThemeIcon('symbol-module'),
-    children: computed.value.map((item: any) => {
-      const label = item.key.name
-      return {
-        label,
-        iconPath: new vscode.ThemeIcon('symbol-method'),
-        command: {
-          title: label,
-          command: 'function-quick-locking.jump',
-          arguments: [item.loc, baseLine],
-        },
-      }
-    }),
-  })
-  context.subscriptions.push(registerCommand('function-quick-locking.jump', (data, baseLine) => {
-    jumpToLine(data.start.line + baseLine - 1)
-  }))
-  const provider = new TreeProvider(treeData)
-  context.subscriptions.push(vscode.window.registerTreeDataProvider('function-quick-locking.id', provider))
+  return treeData
 }
